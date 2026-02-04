f7f5335b660df0f0a6ccfa0f5e1b7facabbf38a1
liguozhou <liguozhou@jswplay.com>
2026-02-04 20:09:57 +0800

    贷款记录-报错修复2

--- store/migration/postgres/0.26/07__loan_repayment_terms.sql
+++ store/migration/postgres/0.26/07__loan_repayment_terms.sql
@@ -1,3 +1,3 @@
--- Deprecated: repayment_periods is now created in loan schema.
+-- Deprecated: repayment_periods/repayment_method are now created in loan schema.
 SELECT 1;
 ALTER TABLE loan ADD COLUMN repayment_method INTEGER NOT NULL DEFAULT 0;
