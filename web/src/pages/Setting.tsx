4dde29a2c3ea0dcdc6fb6f795a4fc642790daf8b
liguozhou <liguozhou@jswplay.com>
2026-03-12 10:12:18 +0800

    个人信息改成直角 新增测试用例流程

--- web/src/pages/Setting.tsx
+++ web/src/pages/Setting.tsx
@@ -116,7 +116,7 @@ const Setting = () => {
     <section className="@container w-full max-w-5xl min-h-full flex flex-col justify-start items-start sm:pt-3 md:pt-6 pb-8">
       {!sm && <MobileHeader />}
       <div className="w-full px-4 sm:px-6">
-        <div className="w-full border border-border flex flex-row justify-start items-start px-4 py-3 rounded-xl bg-background text-muted-foreground">
+        <div className="w-full border border-border flex flex-row justify-start items-start px-4 py-3 bg-background text-muted-foreground">
           {sm && (
             <div className="flex flex-col justify-start items-start w-40 h-auto shrink-0 py-2">
               <span className="text-sm mt-0.5 pl-3 font-mono select-none text-muted-foreground">{t("common.basic")}</span>
